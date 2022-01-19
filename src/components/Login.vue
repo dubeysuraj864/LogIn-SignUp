218d500059f563b4b589dd9950fa5687f83da846
Suraj Dubey <dubeysuraj864@gmail.com>
2022-01-19 23:57:21 +0530

    push

---
+++ src/components/Login.vue
@@ -0,0 +1,34 @@
+<template>
+    <div>
+        <h1>Vue Form</h1>
+        <input id="myInput" type="username" placeholder="username"  v-model="loginForm.username" /> <br> <br>
+        <input id="myInput" type="password" placeholder="password"    v-model="loginForm.password" /> <br> <br> <br>
+        <button v-on:click="loginData">Log In</button>
+    </div>
+</template>
+
+<script>
+export default{
+    name: 'Login',
+    methods:{
+        LoginData(){
+            console.log("logindata")
+        }
+    },
+    data(){
+        return{
+            loginForm:{
+                username: null,
+                password: null,
+            }
+        }
+    }
+}
+</script>
+    
+<style>
+    #myInput{
+        width: 50%;
+        height: 20px;
+    }
+</style>
